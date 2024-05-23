7ad857acb771d5af7ebadc959d105c9493bbf26e
snewkovits <adamhunxd@gmail.com>
2024-05-23 18:35:17 +0200

    fantasy

---
+++ frontend/src/Components/ItemSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ItemSection = () => {
+    return (
+        <section classname='item-section'>
+            <ItemSection/>
+        </section>
+    )
+}
+
+export default ItemSection;
